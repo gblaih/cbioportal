5c5280f7e5973400118568191c8ad94d4ad385c5
Bryan Lai <laib1@mskcc.org>
2023-05-22 15:51:49 -0400

    add mock data and config for clickhouse testing on filtered samples and mutated genes

--- core/src/main/java/org/mskcc/cbio/portal/dao/ClickhouseJdbcDataSource.java
+++ core/src/main/java/org/mskcc/cbio/portal/dao/ClickhouseJdbcDataSource.java
@@ -4,15 +4,12 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.mskcc.cbio.portal.util.GlobalProperties;
 
 public class ClickhouseJdbcDataSource extends BasicDataSource {
-    
+
     public ClickhouseJdbcDataSource() {
-        String connectionString = GlobalProperties.getProperty("db.clickhouse.connection_string");
-        String user = GlobalProperties.getProperty("db.user");
-        String password = GlobalProperties.getProperty("db.password");
         this.setDriverClassName("com.clickhouse.jdbc.ClickHouseDriver");
-        this.setUsername(user);
-        this.setPassword(password);
-        this.setUrl(connectionString);
+        this.setUsername("cbio");
+        this.setPassword("P@ssword1");
+        this.setUrl("jdbc:clickhouse://localhost:8123/cbioportal");
         this.setJmxName("org.cbioportal:DataSource=clickhouse");
     }
 }
