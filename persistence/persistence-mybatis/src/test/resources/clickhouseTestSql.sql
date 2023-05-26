50a64f3579df82911d322b85080c9d30d52cba70
Bryan Lai <laib1@mskcc.org>
2023-05-26 15:45:53 -0400

    update mock data

--- persistence/persistence-mybatis/src/test/resources/clickhouseTestSql.sql
+++ persistence/persistence-mybatis/src/test/resources/clickhouseTestSql.sql
@@ -8,9 +8,9 @@ INSERT INTO patient_clinical_attribute_numeric (patient_unique_id, attribute_nam
 INSERT INTO patient_clinical_attribute_numeric (patient_unique_id, attribute_name, attribute_value, cancer_study_identifier) VALUES ('msk_ch_2020_P-0000023', 'TIME_FROM_DX_TO_SEQ', 245, 'msk_ch_2020');
 INSERT INTO patient_clinical_attribute_numeric (patient_unique_id, attribute_name, attribute_value, cancer_study_identifier) VALUES ('msk_ch_2020_P-0000023', 'TIME_TO_BLOOD_DRAW_FROM_TX', 166, 'msk_ch_2020');
 
-INSERT INTO sample (sample_unique_id, sample_stable_id, patient_unique_id, patient_stable_id, cancer_study_identifier) VALUES ('msk_ch_2020_P-0000004-N01', 'P-0000004-N01', 'msk_ch_2020_P-0000004', 'P-0000004', 'msk_ch_2020');
-INSERT INTO sample (sample_unique_id, sample_stable_id, patient_unique_id, patient_stable_id, cancer_study_identifier) VALUES ('msk_ch_2020_P-0000015-N01', 'P-0000015-N01', 'msk_ch_2020_P-0000015', 'P-0000015', 'msk_ch_2020');
-INSERT INTO sample (sample_unique_id, sample_stable_id, patient_unique_id, patient_stable_id, cancer_study_identifier) VALUES ('msk_ch_2020_P-0000023-N01', 'P-0000023-N01', 'msk_ch_2020_P-0000023', 'P-0000023', 'msk_ch_2020');
+INSERT INTO sample (sample_unique_id, sample_unique_id_base64, sample_stable_id, patient_unique_id, patient_unique_id_base64, patient_stable_id, cancer_study_identifier) VALUES ('msk_ch_2020_P-0000004-N01', '', 'P-0000004-N01', 'msk_ch_2020_P-0000004', '', 'P-0000004', 'msk_ch_2020');
+INSERT INTO sample (sample_unique_id, sample_unique_id_base64, sample_stable_id, patient_unique_id, patient_unique_id_base64, patient_stable_id, cancer_study_identifier) VALUES ('msk_ch_2020_P-0000015-N01', '', 'P-0000015-N01', 'msk_ch_2020_P-0000015', '', 'P-0000015', 'msk_ch_2020');
+INSERT INTO sample (sample_unique_id, sample_unique_id_base64, sample_stable_id, patient_unique_id, patient_unique_id_base64, patient_stable_id, cancer_study_identifier) VALUES ('msk_ch_2020_P-0000023-N01', '', 'P-0000023-N01', 'msk_ch_2020_P-0000023', '', 'P-0000023', 'msk_ch_2020');
 -- 
 INSERT INTO genomic_event (sample_unique_id, variant, hugo_gene_symbol, gene_panel_stable_id, cancer_study_identifier, genetic_profile_stable_id) VALUES ('msk_ch_2020_P-0000004-N01', 'p.R1051Q', 'KDR', '', 'msk_ch_2020', 'msk_ch_2020_mutations');
 INSERT INTO genomic_event (sample_unique_id, variant, hugo_gene_symbol, gene_panel_stable_id, cancer_study_identifier, genetic_profile_stable_id) VALUES ('msk_ch_2020_P-0000004-N01', 'p.T1884I', 'TET2', '', 'msk_ch_2020', 'msk_ch_2020_mutations');
